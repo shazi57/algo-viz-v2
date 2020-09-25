f8addefef648692dd14c8f5919aeac38e27b4bcd
taewoo <tary82504@berkeley.edu>
2020-09-24 22:37:17 -0700

    testing github pages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => (<h1>hello world okay? what about now? really?</h1>);
+const App = () => (<h1>hello world okay? what about now? really? github pages testing 1212</h1>);
 
 export default hot(module)(App);
