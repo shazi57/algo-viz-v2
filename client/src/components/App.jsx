1267cd1b7d2fb8b4678e6c5f894645e9cfe024bd
taewoo <tary82504@berkeley.edu>
2020-10-02 22:05:46 -0700

    implemented button name change functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,9 +14,12 @@ class App extends React.Component {
       canvasCleared: false,
       alertWindow: false,
       alertMessage: null,
+      renderAnimation: false,
+      chosenAlgo: null,
     };
     this.onCreateClicked = this.onCreateClicked.bind(this);
     this.onClearClicked = this.onClearClicked.bind(this);
+    this.onSortClicked = this.onSortClicked.bind(this);
   }
 
   onCreateClicked(size) {
@@ -50,6 +53,30 @@ class App extends React.Component {
     });
   }
 
+  onSortClicked(algo) {
+    const { canvasCreated } = this.state;
+    if (!canvasCreated) {
+      this.setState({
+        alertWindow: true,
+        alertMessage: 'You should create a canvas before rendering animation',
+        renderAnimation: false,
+        chosenAlgo: algo,
+      });
+    } else if (algo === 'Algorithms') {
+      this.setState({
+        alertWindow: true,
+        alertMessage: 'You should choose a valid algorithm before rendering animation',
+        renderAnimation: false,
+        chosenAlgo: null,
+      });
+    } else {
+      this.setState({
+        renderAnimation: true,
+        chosenAlgo: algo,
+      });
+    }
+  }
+
   render() {
     const {
       size,
@@ -57,13 +84,19 @@ class App extends React.Component {
       alertWindow,
       alertMessage,
       canvasCleared,
+      renderAnimation,
+      chosenAlgo,
     } = this.state;
+
+    const canRenderAnimation = canvasCreated && chosenAlgo !== null && renderAnimation;
+    
     return (
       <div className="container">
         <NavBar
           onCreateClicked={this.onCreateClicked}
           onSizeConfirmed={this.onSizeConfirmed}
           onClearClicked={this.onClearClicked}
+          onSortClicked={this.onSortClicked}
         />
         {(alertWindow ? <Alert severity="error">{alertMessage}</Alert> : null)}
         <Canvas size={size} canvasCreated={canvasCreated} canvasCleared={canvasCleared} />
