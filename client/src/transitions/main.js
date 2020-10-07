a4df1ea006e2e7f91613e4721f4e6f92d0e44c00
taewoo <tary82504@berkeley.edu>
2020-10-07 14:40:08 -0700

    completed animation renderer main sorting visualization

--- client/src/transitions/main.js
+++ client/src/transitions/main.js
@@ -2,10 +2,12 @@ import switchAnimation from './swithAnimation';
 
 const renderAnimation = (config, algorithm) => {
   const { canvas, data, barInterval } = config;
+  const size = data.length;
+  const animationSpeed = 8000 / size;
   const mappings = algorithm(data);
   const starter = mappings.shift();
   const [from, to] = starter;
-  switchAnimation(canvas, mappings, barInterval, from, to);
+  switchAnimation(canvas, mappings, animationSpeed, barInterval, from, to);
 };
 
 export default renderAnimation;
--- client/src/transitions/swithAnimation.js
+++ client/src/transitions/swithAnimation.js
@@ -1,22 +1,28 @@
-const switchAnimation = (canvas, mappings, barInterval, from, to) => {
+const switchAnimation = (canvas, mappings, animationSpeed, barInterval, from, to) => {
   const fromNode = canvas.select(`#num_${from}`);
   const toNode = canvas.select(`#num_${to}`);
   return fromNode
     .transition()
-    .duration(300)
+    .duration(animationSpeed)
+    .style('fill', 'blue')
     .attr('x', () => 100 + (to) * barInterval)
     .on('end', () => {
       toNode
         .transition()
-        .duration(300)
+        .duration(animationSpeed)
+        .style('fill', 'red')
         .attr('x', () => 100 + (from) * barInterval)
         .on('end', () => {
           const temp = fromNode.attr('id');
-          fromNode.attr('id', () => toNode.attr('id'));
-          toNode.attr('id', () => temp);
+          fromNode
+            .attr('id', () => toNode.attr('id'))
+            .style('fill', 'green');
+          toNode
+            .attr('id', () => temp)
+            .style('fill', 'green');
           if (mappings.length !== 0) {
             const [nextFrom, nextTo] = mappings.shift();
-            switchAnimation(canvas, mappings, barInterval, nextFrom, nextTo);
+            switchAnimation(canvas, mappings, animationSpeed, barInterval, nextFrom, nextTo);
           }
         });
     });
